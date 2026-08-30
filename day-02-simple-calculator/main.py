8439dcc9618e7951530d8d569d343631f226f13b
weeedushi <bhardwajvidushi85@gmail.com>
2026-08-30 21:57:00 +0530

    Basic logic added

--- day-02-simple-calculator/main.py
+++ day-02-simple-calculator/main.py
@@ -1,1 +1,28 @@
-print("hello")
+def calculator(operation, num1, num2):
+    match operation:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        case _:
+            return "Invalid operator"
+
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+except ValueError: 
+    print("Not an integer")
+    exit()
+
+try:
+    operator= str(input("Enter the operator symbol: "))
+except ValueError:
+    print("Not a string")
+    exit()
+
+result = calculator(operator, num1, num2)
+print(result)
