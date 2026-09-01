039a772114cf8a8f8c438885563363f84c6b82cd
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-01 16:03:42 +0530

    Add replay functionality

--- day-01-number-guessing/main.py
+++ day-01-number-guessing/main.py
@@ -1,32 +1,40 @@
 import random
 
-print("Im thinking of a number between 0 and 100")
-actual_number = random.randint(0, 100)
+play='y'
+while play=='y' or play=='Y':
 
-print("You have 7 guesses")
+    print("Im thinking of a number between 0 and 10")
+    actual_number = random.randint(0, 10)
+    print("You have 4 guesses")
 
-count=7
+    count=4
 
-while count > 0 :
-    try: 
-        guess=int(input("Guess the number: "))
-    except ValueError:
-        print("Not a valid number")
-        continue
-    if guess >=0 and guess <=100 :
-        count -= 1
-        if guess < actual_number :
-            print("go higher")
-        elif guess > actual_number :
-            print("go lower")
+    while count > 0:
+        try: 
+            guess=int(input("Guess the number: "))
+        except ValueError:
+            print("Not a valid number")
+            continue
+        if guess >=0 and guess <=10 :
+            count -= 1
+            if guess < actual_number :
+                print("go higher")
+            elif guess > actual_number :
+                print("go lower")
+            else:
+                print("CORRECT!!! YOU WON!!")
+                # count=0
+                break
+            print(f"\nRemaining guess: {count}/7")
         else:
-            print("CORRECT!!! YOU WON!!")
-            break
-        print(f"\nRemaining guess: {count}/7")
-    else:
-        print("Invalid input")
+            print("Invalid input")
        
-else:
-    print("Game Over")
+    else:
+        print("Game Over")
+
+    play=input("Press 'y' to play again: ")
+
+print("Thanks for playing")
+
 
 
