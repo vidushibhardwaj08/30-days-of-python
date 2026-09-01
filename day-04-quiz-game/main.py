6a7d1e38789dc3aa948fc9d170913cc2ba65a8e0
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-01 20:44:11 +0530

    Add core quiz logic

---
+++ day-04-quiz-game/main.py
@@ -0,0 +1,23 @@
+questions={"What is my favorite color":"green", "whats my favorite office character":"kelly", "Whats my favorite number?":5, "What do i like the most?": "reading", "Whats my favorite fruit": "mango"}
+options=[("yellow","blue","pink","green"), ("michael","jim","kelly","dwight"),(5,8,7,2),("swimming","reading","singing","dancing"),("banana","apple","mango","litchi")]
+
+x = list(questions.keys())
+y = list(questions.values())
+
+right=True
+i=0
+score=0
+while right==True and i<5:
+    print("\n")
+    print(f"Question {i+1}:\n{x[i]}")
+    print(f"Options are {options[i]}")
+    answer= input("your answer: ")
+    if answer==y[i]:
+        print("CORRECT\n\n")
+        score+=10
+    else:
+        print(f"WRONG ({y[i]} is the correct answer)\n\n")
+    i+=1
+
+print(f"\nYour score: {score}")
+print("Quiz over")
