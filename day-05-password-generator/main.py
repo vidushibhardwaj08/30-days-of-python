f7fbe1025ab527573d3b36894c7fe1d817f868d1
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-02 13:06:44 +0530

    Add basic logic

--- day-05-password-generator/main.py
+++ day-05-password-generator/main.py
@@ -1,3 +1,5 @@
+import random
+
 print("\nPASSWORD GENERATOR\n")
 
 try:
@@ -5,4 +7,11 @@ try:
 except ValueError:
     print("Not a number")
     exit()
-characters=["abcdefghijklmnopqrstuvwxyz1234567890@#_"]
+characters=[("abcdefghijklmnopqrstuvwxyz"),("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),("1234567890"),("@#_")]
+
+password=""
+while len(password)<length:
+    i= random.randint(0,3)
+    password= password+random.choice(characters[i])
+
+print(password)
