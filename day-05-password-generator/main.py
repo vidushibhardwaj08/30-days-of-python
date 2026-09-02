d223aaf318acac23d4a044b1332511a3e23be707
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-02 18:19:54 +0530

    Minimum length set

--- day-05-password-generator/main.py
+++ day-05-password-generator/main.py
@@ -1,17 +1,29 @@
 import random
 
 print("\nPASSWORD GENERATOR\n")
+while True:
+    password=""
+    try:
+        length=int(input("Enter password length(more than 4): "))
+        if length<4:
+            print("please enter a number more than minimum length")
+            continue
+    except ValueError:
+        print("Not a number")
+        continue
+    characters=[("abcdefghijklmnopqrstuvwxyz"),("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),("1234567890"),("@#_")]
 
-try:
-    length=int(input("Enter password length: "))
-except ValueError:
-    print("Not a number")
-    exit()
-characters=[("abcdefghijklmnopqrstuvwxyz"),("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),("1234567890"),("@#_")]
+    i=0
+    while len(password)<length:
+        if i>3:
+            i=0
+        password= password+random.choice(characters[i])
+        i=i+1
+    else:
+        break
 
-password=""
-while len(password)<length:
-    i= random.randint(0,3)
-    password= password+random.choice(characters[i])
-
-print(password)
+if password!="":
+    pass_list = list(password)
+    random.shuffle(pass_list)
+    new_password="".join(pass_list)
+    print(new_password)
