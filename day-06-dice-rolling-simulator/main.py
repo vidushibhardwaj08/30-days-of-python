7cbd7da0036572892d6fa41c17be8bbdad85b53b
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-03 15:21:23 +0530

    Add user input and while loop

--- day-06-dice-rolling-simulator/main.py
+++ day-06-dice-rolling-simulator/main.py
@@ -4,5 +4,12 @@ def roll_die():
     x= random.randint(1,6)
     return x
 
-chance = roll_die()
-print(chance)
+print("DICE ROLLING SIMULATOR")
+answer= input("Do you want to roll the dice? (y/n): ")
+
+while answer.lower()=='y':
+    result=roll_die()
+    print(f"You rolled: {result}")
+    answer= input("Do you want to roll the dice? (y/n): ")
+
+print("Thanks for playing!!")
