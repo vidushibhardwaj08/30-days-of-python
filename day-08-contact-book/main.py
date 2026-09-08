19dcfebb013f26f41362554d0d07573a45cd71eb
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-08 21:50:50 +0530

    add contact function

--- day-08-contact-book/main.py
+++ day-08-contact-book/main.py
@@ -1,4 +1,17 @@
 print("\nCONTACT BOOK\n")
+contacts = {}
+
+def addContact():
+    name = input("Enter contact name: ")
+    phone = input("Enter phone number: ")
+    email = input("Enter email: ")
+
+    contacts[name] = {
+        "phone": phone,
+        "email": email
+    }
+
+    print("Contact added successfully")
 
 while True:
     print("""    1. Add Contact
