97d10261ed8fbc878dec74ec06cb73e3a4ce1858
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-08 21:38:12 +0530

    add menu loop and user choice

--- day-08-contact-book/main.py
+++ day-08-contact-book/main.py
@@ -1,7 +1,14 @@
 print("\nCONTACT BOOK\n")
-print("""1. Add Contact
-2. View All Contacts
-3. Search Contact
-4. Update Contact
-5. Delete Contact
-6. Exit\n""")
+
+while True:
+    print("""    1. Add Contact
+    2. View All Contacts
+    3. Search Contact
+    4. Update Contact
+    5. Delete Contact
+    6. Exit\n""")
+
+    userInput = int(input("Choose an option: "))
+
+    if userInput==6:
+        break
