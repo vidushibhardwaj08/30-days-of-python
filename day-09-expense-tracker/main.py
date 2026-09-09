6f3194939ab2f19b8e25951aac7d7dd31877874a
weeedushi <bhardwajvidushi85@gmail.com>
2026-09-09 22:21:26 +0530

    display user options

---
+++ day-09-expense-tracker/main.py
@@ -0,0 +1,9 @@
+print("\nEXPENSE TRACKER\n")
+
+print("Select an option: " \
+"1. Add an expense" \
+"2. View all expenses" \
+"3. Calculate total expenses" \
+"4. View expenses by category" \
+"5. Delete an expense" \
+"6. Exit")
